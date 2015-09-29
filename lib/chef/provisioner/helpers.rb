590252e5d3acb81e71e15ad6a2787517e0991056
double-z <zackzondlo@gmail.com>
2015-09-29 16:19:57 -0400

    helpers

--- lib/chef/provisioner/helpers.rb
+++ lib/chef/provisioner/helpers.rb
@@ -1,6 +1,13 @@
+# require 'chef/provisioner/helpers/paths'
+# require 'chef/provisioner/helpers/paths'
+# require 'chef/provisioner/helpers/resources'
+
 class Chef
   module Provisioner
     module Helpers
+      # extend class Chef::Provisioner::Paths
+      # extend class Chef::Provisioner::Resources
+
       def self.deep_hashify(machine_options)
         if machine_options.respond_to?(:to_hash)
           hash = machine_options.to_hash
@@ -16,6 +23,23 @@ class Chef
           }
         end
       end
+
+      def deep_hashify(machine_options)
+        if machine_options.respond_to?(:to_hash)
+          hash = machine_options.to_hash
+
+          hash.inject({}){|result, (key, value)|
+            if value.respond_to?(:to_hash)
+              new_value = deep_hashify(value)
+            else
+              new_value = value
+            end
+            result[key] = new_value
+            result
+          }
+        end
+      end
+
     end
   end
 end
