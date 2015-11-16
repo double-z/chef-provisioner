3be393f6216f355218364ec9aa321fb93b9f91c5
zz <zz@zzs-MacBook-Pro.local>
2015-11-16 00:08:32 -0500

    added

--- lib/chef/resources/chef_platform_provision.rb
+++ lib/chef/resources/chef_platform_provision.rb
@@ -25,9 +25,7 @@ class Chef
 
       self.resource_name = 'chef_platform_provision'
 
-      # ready and destroy_all along with cheffish attrs
-      # are inhereted from machine resource
-      actions :_test_ready, :reconfigure, :generate_config, :_push_config
+      actions :_test_ready, :reconfigure, :generate_config, :_push_config, :destroy_all
       default_action :nothing
 
       attribute :init_time, :kind_of => String, :default => Time.now.strftime("%Y%m%d%H%M%S")
@@ -49,3 +47,57 @@ class Chef
     end
   end
 end
+
+
+
+# require 'cheffish'
+# require 'chef/provisioning'
+# require 'chef/resource/machine'
+
+# class Chef
+#   class Resource
+#     class ChefPlatformProvision < Chef::Resource::Machine
+
+#       def initialize(*args)
+#         # We are using the name as the policy group 
+#         # but need the name to be the node_name of
+#         # the provisioner node so we swap them out 
+#         args.each do |k|
+#           policy_group(k) if k.kind_of?(String)
+#         end
+#         args.collect! { |element|
+#           (element.kind_of?(String)) ? Chef::Config[:node_name] : element
+#         }
+#         super
+#       end
+
+#       def policy_group(arg_val = nil)
+#         @policy_group ||= arg_val
+#       end
+
+#       self.resource_name = 'chef_platform_provision'
+
+#       # ready and destroy_all along with cheffish attrs
+#       # are inhereted from machine resource
+#       actions :_test_ready, :reconfigure, :generate_config, :_push_config
+#       default_action :nothing
+
+#       attribute :init_time, :kind_of => String, :default => Time.now.strftime("%Y%m%d%H%M%S")
+#       attribute :policy_group, :kind_of => String
+
+#       attribute :driver_name, :kind_of => String
+
+#       attribute :chef_server_topology, :kind_of => String
+#       attribute :chef_server_version
+#       attribute :chef_server_api_fqdn, :kind_of => String
+#       attribute :chef_server_configuration, :kind_of => Hash
+
+#       attribute :analytics_version
+#       attribute :analytics_api_fqdn, :kind_of => String
+#       attribute :analytics_configuration, :kind_of => Hash
+
+#       attribute :nodes, :kind_of => Array
+
+#     end
+#   end
+# end
--- recipes/reconfigure.rb
+++ recipes/reconfigure.rb
@@ -6,6 +6,49 @@
 include_recipe 'chef-provisioner::default'
 
 chef_platform_provision "prod" do
+  driver_name 'vagrant'
+  chef_server_topology "tier"
+  chef_server_frontends 2
+  platform_domain "mydomain.com"
+  chef_server_hostname_stub
+  manage_hostname_stub
+  reporting_hostname_stub
+end
+
+chef_platform_provision "prod" do
+  driver_name 'vagrant'
+  chef_server_topology "tier"
+  chef_server_frontends 2
+  manage_fqdn 
+  reporting_fqdn 
+  chef_server_fqdn 
+  analytics_version 1.1.6
+  manage_version 1.21.0
+  reporting_version 1.5.1
+  with_analytics true
+  with_manage true
+  with_reporting true
+end
+
+chef_platform_provision "prod" do
+  platform_domain "mydomain.com"
+  driver_name 'vagrant'
+  chef_server_topology "tier"
+  chef_server_frontends 2
+  with_analytics true
+  with_manage true
+  with_reporting true
+end
+
+chef_platform_provision "prod" do
+  driver_name 'vagrant'
+  chef_server_topology "tier"
+  node_count 500
+  convergence_rate 30
+  with_analytics 1.1.6
+end
+
+chef_platform_provision "prod" do
   action :reconfigure
   driver_name 'vagrant'
   chef_server_topology "tier"
@@ -45,6 +88,89 @@ chef_platform_provision "prod" do
         "hipchat_room" => 'Chef Notification'
       }
     })
+  nodes([
+      {
+        "node_name" => "backend1",
+        "service" => "chef_server",
+        "fqdn" =>"backend1.ubuntu.vagrant",
+        "interface" => "eth1",
+        "machine_options_ipaddress" => "33.33.33.20",
+        "role" => "backend",
+        "bootstrap" => true
+      },
+      {
+        "node_name" => "frontend1",
+        "service" => "chef_server",
+        "fqdn" => "frontend1.ubuntu.vagrant",
+        "interface" => "eth1",
+        "machine_options_ipaddress" => "33.33.33.22",
+        "role" => "frontend",
+        "bootstrap" => false
+      },
+      {
+        "node_name" => "analytics1",
+        "service" => "analytics",
+        "fqdn" => "analytics.ubuntu.vagrant",
+        "interface" => "eth1",
+        "machine_options_ipaddress" => "33.33.33.25",
+        "role" => "analytics",
+        "bootstrap" => false
+      }
+    ])
+end
+
+chef_platform do
+  name "prod"
+  action :reconfigure
+  driver do
+    name 'vagrant'
+  end
+  def chef_server(&block)
+    @chef_server ||= ChefServerType.new
+    @chef_server.instance_eval(&block) if block
+    @chef_server
+  end
+  chef_server do
+    topology "tier"
+    version :latest
+    api_fqdn "chef-server.chefplatform.local"
+    configuration({ 
+      "postgresql" => {
+        "max_connections" => 1501,
+        "log_min_duration_statement" => 500
+      },
+      "opscode_erchef" => {
+        "depsolver_worker_count" => 4,
+        "depsolver_timeout" => 120000,
+        "db_pool_size" => 100
+      },
+      "oc_bifrost" => {
+        "db_pool_size" => 100
+      },
+      "opscode_certificate" => {
+        "num_workers" => 4,
+        "num_certificates_per_worker" => 1000
+      },
+      "oc_chef_authz" => {
+        "http_init_count" => 150,
+        "http_max_count" => 150
+      }
+    })
+  end
+  analytics do
+    version :latest
+    api_fqdn "analytics.chefplatform.local"
+    configuration({
+      "actions_consumer" => {
+        "hipchat_api_token" => "4yZvEWO6gnVhjtl3F6aU12obylgNXzzqmklI5jP0",
+        "hipchat_enabled" => [
+          "prod",
+          "preprod"
+        ],
+        "hipchat_room" => 'Chef Notification'
+      }
+    })
+  end
   nodes([
       {
         "node_name" => "backend1",
